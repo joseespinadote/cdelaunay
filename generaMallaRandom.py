33e535288ee0abd149a846b0485d578f6cc41318
José en el asus zenbook <el.charlie@gmail.com>
2021-06-06 02:36:47 -0400

    generadores de mallas

---
+++ generaMallaRandom.py
@@ -0,0 +1,5 @@
+from random import uniform
+
+for i in range(1,1000,1) :
+    print(uniform(10,90), end=' ')
+    print(uniform(10,90))
---
+++ generaMallaRegular.py
@@ -0,0 +1,9 @@
+xmin = 10
+xmax = 91
+ymin = xmin
+ymax = xmax
+step = 3
+for i in range(xmin,xmax,step) :
+    for j in range(ymin,ymax,step) :
+        print(i, end=' ')
+        print(j)
